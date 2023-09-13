3b4f91f51dbc976b61e298190c934af647790392
Jan Kaleta <kaletajan57@gmail.com>
2023-09-13 16:08:23 +0200

    Modified the insert_sign() method inside Board class - now the signs cannot be added in cell that is not empty. Also added some code in main() to test if the already existing methods are correctly working

--- classes/board.py
+++ classes/board.py
@@ -2,11 +2,17 @@ class Board:
     def __init__(self) -> None:
         self.__board = [["", "", ""], ["", "", ""], ["", "", ""]]
 
-    #Resets board to default state
+    # Resets board to default state
     def reset_board(self) -> None:
         self.__board = [["", "", ""], ["", "", ""], ["", "", ""]]
-    
-    #Inserts to board X or O - basic move in the game    
-    def insert_sign(self, sign: str, row: int, column: int):
-        self.__board[row][column] = sign
 
+    # Inserts to board X or O - basic move in the game
+    def insert_sign(self, sign: str, row: int, column: int) -> None:
+        if self.__board[row][column] == "": 
+            self.__board[row][column] = sign
+
+    #Prints board state in console
+    def show_board(self) -> None:
+        print(self.__board[0])
+        print(self.__board[1])
+        print(self.__board[2])
