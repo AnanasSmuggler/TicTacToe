7a7ff83f74bf0b6e6b4b7560b0071195c9834721
Jan Kaleta <kaletajan57@gmail.com>
2023-09-13 17:12:15 +0200

    Added working method to class Board, that checks whether the given sign won or not

--- classes/board.py
+++ classes/board.py
@@ -1,18 +1,45 @@
 class Board:
     def __init__(self) -> None:
-        self.__board = [["", "", ""], ["", "", ""], ["", "", ""]]
+        self.__board = [["", "", ""], ["", "", ""], ["", "", ""]] #board
+        self.__empty_spaces = 9 #empty spaces left on a board
 
     # Resets board to default state
     def reset_board(self) -> None:
         self.__board = [["", "", ""], ["", "", ""], ["", "", ""]]
+        self.__empty_spaces = 9
 
     # Inserts to board X or O - basic move in the game
     def insert_sign(self, sign: str, row: int, column: int) -> None:
-        if self.__board[row][column] == "": 
+        if self.__board[row][column] == "":
             self.__board[row][column] = sign
+            self.__empty_spaces -= 1
+        
+        if self.__empty_spaces <= 4:
+            if self.check_win(sign):
+                print(f'Gratulacje! Grę zwycięża {sign}!')
 
     #Prints board state in console
     def show_board(self) -> None:
         print(self.__board[0])
         print(self.__board[1])
         print(self.__board[2])
+
+    #Checks if the given sign has won
+    def check_win(self, sign: str) -> bool:
+        return self.__check_row(sign) or self.__check_column(sign) or self.__check_diagonal(sign)
+
+    #Private helper for check_win - checks every row
+    def __check_row(self, sign: str) -> bool:
+        winning_row = [sign, sign, sign]
+        return self.__board[0] == winning_row or self.__board[1] == winning_row or self.__board[2] == winning_row
+
+    #Private helper for check_win - checks every column
+    def __check_column(self, sign: str) -> bool:
+        for col in range(3):
+            if self.__board[0][col] == self.__board[1][col] == self.__board[2][col] == sign:
+                return True
+        return False
+
+    #Private helper for check_win - checks every diagonal
+    def __check_diagonal(self, sign: str) -> bool:
+        return self.__board[0][0] == self.__board[1][1] == self.__board[2][2] == sign or self.__board[0][2] == self.__board[1][1] == self.__board[2][0] == sign
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import classes.board
 
+
 def main() -> None:
     print("Hello World!")
     board = classes.board.Board()
-    board.insert_sign("X", 0, 0)
-    board.show_board()
+    board.insert_sign("X", 2, 0)
+    board.insert_sign("O", 0, 1)
+    board.insert_sign("X", 2, 1)
     board.insert_sign("O", 0, 0)
-    board.insert_sign("O", 1, 1)
-    board.insert_sign("X", 0, 1)
+    board.insert_sign("X", 2, 2)
     board.show_board()
-    
+
 
 if __name__ == "__main__":
     main()
