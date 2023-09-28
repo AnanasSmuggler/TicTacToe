4f3e97965b69a687b546d902a564072ab03a8242
Jan Kaleta <kaletajan57@gmail.com>
2023-09-28 22:06:41 +0200

    Created basic menu in GUI class

--- classes/gui.py
+++ classes/gui.py
@@ -1,5 +1,40 @@
-import customtkinter
+import customtkinter as ctk
+import sys
 
 class GUI:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, root) -> None:
+        self.root = root
+        self.root.title("Tic Tac Toe")
+        self.root.geometry("400x400")
+        self.create_menu()
+        
+    def create_menu(self) -> None:
+        self.title_label = ctk.CTkLabel(self.root, text="TIC TAC TOE", font=("Helvetica", 32))
+        self.title_label.pack(pady=25)
+        
+        self.name = ctk.CTkEntry(self.root, text="PLAY", command=self.play_button_click)
+        self.playButton.pack(pady=25)
+
+        self.exitButton = ctk.CTkButton(self.root, text="EXIT", command=self.exit_button_click)
+        self.exitButton.pack(pady=25)
+    
+    def create_play_menu(self) -> None:
+        self.title_label = ctk.CTkLabel(self.root, text="TIC TAC TOE", font=("Helvetica", 32))
+        self.title_label.pack(pady=25)
+        
+        self.playButton = ctk.CTkButton(self.root, text="PLAY", command=self.play_button_click)
+        self.playButton.pack(pady=25)
+
+        self.exitButton = ctk.CTkButton(self.root, text="EXIT", command=self.exit_button_click)
+        self.exitButton.pack(pady=25)
+    
+    def clear_content(self) -> None:
+        for widget in self.root.winfo_children():
+            widget.destroy()
+        
+    def play_button_click(self) -> None:
+        self.clear_content()
+        self.create_play_menu()
+
+    def exit_button_click(self) -> None:
+        sys.exit(1)
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import classes.game as g
+import classes.gui as gui
+import customtkinter as ctk
 
 
 def main() -> None:
-    game = g.Game()
-    game.prepare_game()
-
+    #game = g.Game()
+    #game.prepare_game()
+    root = ctk.CTk()
+    my_window = gui.GUI(root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
