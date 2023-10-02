4a502185f2b9f810d87c365a74308adfe83e5c3d
Jan Kaleta <kaletajan57@gmail.com>
2023-10-02 21:53:11 +0200

    Added the start game form and simple alert

--- classes/gui.py
+++ classes/gui.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 import sys
+from CTkMessagebox import CTkMessagebox
 
 class GUI:
     def __init__(self, root) -> None:
@@ -9,24 +10,42 @@ class GUI:
         self.create_menu()
         
     def create_menu(self) -> None:
-        self.title_label = ctk.CTkLabel(self.root, text="TIC TAC TOE", font=("Helvetica", 32))
-        self.title_label.pack(pady=25)
-        
-        self.name = ctk.CTkEntry(self.root, text="PLAY", command=self.play_button_click)
-        self.playButton.pack(pady=25)
-
-        self.exitButton = ctk.CTkButton(self.root, text="EXIT", command=self.exit_button_click)
-        self.exitButton.pack(pady=25)
-    
-    def create_play_menu(self) -> None:
-        self.title_label = ctk.CTkLabel(self.root, text="TIC TAC TOE", font=("Helvetica", 32))
-        self.title_label.pack(pady=25)
+        self.titleLabel = ctk.CTkLabel(self.root, text="TIC TAC TOE", font=("Helvetica", 32))
+        self.titleLabel.pack(pady=25)
         
         self.playButton = ctk.CTkButton(self.root, text="PLAY", command=self.play_button_click)
         self.playButton.pack(pady=25)
 
         self.exitButton = ctk.CTkButton(self.root, text="EXIT", command=self.exit_button_click)
         self.exitButton.pack(pady=25)
+    
+    def create_play_menu(self) -> None:
+        self.titleLabel = ctk.CTkLabel(self.root, text="TIC TAC TOE", font=("Helvetica", 32))
+        self.titleLabel.pack(pady=25)
+        
+        self.labelNameF = ctk.CTkLabel(self.root, text="First Player:")
+        self.labelNameF.pack()
+
+        self.textInputNameF = ctk.CTkEntry(self.root)
+        self.textInputNameF.pack(pady=10)
+        
+        self.labelNameS = ctk.CTkLabel(self.root, text="Second Player:")
+        self.labelNameS.pack()
+
+        self.textInputNameS = ctk.CTkEntry(self.root)
+        self.textInputNameS.pack(pady=10)
+        
+        self.labelRounds = ctk.CTkLabel(self.root, text="How many rounds to win:")
+        self.labelRounds.pack()
+
+        self.textInputRounds = ctk.CTkEntry(self.root)
+        self.textInputRounds.pack(pady=10)
+        
+        self.submitButton = ctk.CTkButton(self.root, text="START GAME", command=self.start_button_click)
+        self.submitButton.pack(pady=5)
+        
+        self.backButton = ctk.CTkButton(self.root, text="GO BACK", command=self.back_button_click)
+        self.backButton.pack(pady=5)
     
     def clear_content(self) -> None:
         for widget in self.root.winfo_children():
@@ -35,6 +54,20 @@ class GUI:
     def play_button_click(self) -> None:
         self.clear_content()
         self.create_play_menu()
+
+    def start_button_click(self) -> None:
+        fName = self.textInputNameF.get()
+        sName = self.textInputNameS.get()
+        rounds = self.textInputRounds.get()
+        if fName.strip() and sName.strip() and rounds.strip():
+            self.clear_content()
+            print(f'{fName} vs {sName} | {rounds}')
+        else:
+            CTkMessagebox(title="Error", message="Wrong input data!!!", icon="cancel")
+
+    def back_button_click(self) -> None:
+        self.clear_content()
+        self.create_menu()
 
     def exit_button_click(self) -> None:
         sys.exit(1)
